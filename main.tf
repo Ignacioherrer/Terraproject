6f80210c2a153a75af078f64b4b1e585ad55418d
ignacioherrer <ignacioherrer@hotmail.com>
2024-09-30 20:56:15 +0100

    added igw and rt

--- main.tf
+++ main.tf
@@ -71,3 +71,17 @@ resource "aws_internet_gateway" "terraform-igw" {
     Name = "${var.tag["Name"]}-igw"
   }
 }
+
+resource "aws_route_table" "r" {
+  vpc_id = aws_vpc.terraform-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.terraform-igw.id
+  }
+
+  
+  tags = {
+    Name = "${var.tag["Name"]}-rt"
+  }
+}

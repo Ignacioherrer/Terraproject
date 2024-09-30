60ac8fb75c08c1e2bdce05c077ea94b06e88dd12
ignacioherrer <ignacioherrer@hotmail.com>
2024-09-30 19:56:04 +0100

    subnet within default vpc and output with vpc id

--- main.tf
+++ main.tf
@@ -1,27 +1,35 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.69.0"
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
   # Configuration options
 }
 
 variable "vpc_cidr_block" {
-    type = string
+  type = string
 }
 
 variable "tag" {
-    type = map(string)
+  type = map(string)
 }
-variable "subnet_cidr_block_1" { 
+
+variable "subnet_cidr_block_1" {
+  type = string
 }
-variable "subnet_cidr_block_2" { 
+
+variable "subnet_cidr_block_2" {
+  type = string
+}
+
+variable "subnet_cidr_block_3" {
+  type = string
 }
 
 resource "aws_vpc" "terraform-vpc" {
@@ -33,13 +41,25 @@ resource "aws_vpc" "terraform-vpc" {
 resource "aws_subnet" "terraform-subnet-1" {
   vpc_id     = aws_vpc.terraform-vpc.id
   cidr_block = var.subnet_cidr_block_1
-  tags = var.tag
+  tags       = var.tag
 }
 
 resource "aws_subnet" "terraform-subnet-2" {
   vpc_id     = aws_vpc.terraform-vpc.id
   cidr_block = var.subnet_cidr_block_2
-  tags = var.tag
+  tags       = var.tag
 }
-    
-    
+
+data "aws_vpc" "default_vpc" {
+  default = true
+}
+
+resource "aws_subnet" "terraform-subnet-3" {
+  vpc_id     = data.aws_vpc.default_vpc.id
+  cidr_block = var.subnet_cidr_block_3
+  tags       = var.tag
+}
+
+output "terraform-vpc-id" {
+  value = aws_vpc.terraform-vpc.id
+}
